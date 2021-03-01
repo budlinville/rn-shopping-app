2d8e2128d74fea2e99c12fc71c2bf3633ffc1443
bud linville <budlinville@gmail.com>
2021-03-01 17:26:18 -0600

    added fonts

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -14,6 +14,8 @@ const ProductsNavigator = createStackNavigator({
 		headerStyle: {
 			backgroundColor: Platform.OS === 'android' ? Colors.primary : ''
 		},
+		headerTitleStyle: { fontFamily: 'open-sans-bold' },
+		headerBackTitleStyle: { fontFamily: 'open-sanas' },
 		headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
 	}
 });
