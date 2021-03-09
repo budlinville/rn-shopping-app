0b5a89775ea72b44581237e44249da24952d3516
bud linville <budlinville@gmail.com>
2021-03-09 16:05:49 -0600

    Added product fetching

--- store/actions/products.js
+++ store/actions/products.js
@@ -1,12 +1,33 @@
+import Product from "../../models/product";
+
 export const DELETE_PRODUCT = 'DELETE_PRODUCT';
 export const CREATE_PRODUCT = 'CREATE_PRODUCT';
 export const UPDATE_PRODUCT = 'UPDATE_PRODUCT';
+export const SET_PRODUCTS = 'SET_PRODUCTS';
+
+export const fetchProducts = () => {
+	return async dispatch => {
+		// can write any async code I want now
+		const response = await fetch('https://rn-complete-guide-785b1-default-rtdb.firebaseio.com/products.json');
+		const respData = await response.json();
+		console.log(respData);
+		console.log('entires', Object.entries(respData));
+		const loadedProducts = Object.entries(respData).map((entry) => new Product(
+			entry[0],	// key
+			'u1',
+			entry[1].title,
+			entry[1].imageUrl,
+			entry[1].description,
+			entry[1].price
+		));
+		dispatch({ type: SET_PRODUCTS, products: loadedProducts })
+	};
+};
 
 export const deleteProduct = pid => ({ type: DELETE_PRODUCT, pid });
 
 export const createProduct = ( title, description, imageUrl, price ) => {
 	return async dispatch => {
-		// can write any async code I want now
 		const response = await fetch('https://rn-complete-guide-785b1-default-rtdb.firebaseio.com/products.json', {
 			method: 'POST',
 			headers: { 'Content-Type': 'application/json' },
--- store/reducers/products.js
+++ store/reducers/products.js
@@ -1,6 +1,6 @@
 import PRODUCTS from '../../data/dummy-data';
 import Product from '../../models/product';
-import { DELETE_PRODUCT, CREATE_PRODUCT, UPDATE_PRODUCT } from '../actions/products';
+import { SET_PRODUCTS, DELETE_PRODUCT, CREATE_PRODUCT, UPDATE_PRODUCT } from '../actions/products';
 
 const initialState = {
 	availableProducts: PRODUCTS,
@@ -9,6 +9,11 @@ const initialState = {
 
 export default (state=initialState, action) => {
 	switch (action.type) {
+		case SET_PRODUCTS:
+			return {
+				availableProducts: action.products,
+				userProducts: action.products.filter(prod => prod.ownerId === 'u1')
+			};
 		case CREATE_PRODUCT: {
 			const { id, title, imageUrl, description, price } = action.productData;
 			const newProduct = new Product(id, 'u1', title, imageUrl, description, price);
