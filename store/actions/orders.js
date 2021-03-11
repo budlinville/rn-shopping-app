d14f6702d3fee7987008fa75a4e619dc62e22bf1
bud linville <budlinville@gmail.com>
2021-03-10 19:10:11 -0600

    Added firebase storage for orders

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -1,9 +1,32 @@
 export const ADD_ORDER = 'ADD_ORDER';
 
-export const addOrder = (cartItems, totalAmount) => ({
-	type: ADD_ORDER,
-	orderData: {
-		items: cartItems,
-		amount: totalAmount
-	}
-});
+export const addOrder = (cartItems, totalAmount) => {
+	return async dispatch => {
+		const date = new Date();
+		const response = await fetch('https://rn-complete-guide-785b1-default-rtdb.firebaseio.com/orders/u1.json', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({
+				cartItems,
+				totalAmount,
+				date: date.toISOString()
+			})
+		});
+
+		if (!response.ok) {
+			throw new Error('Something went wrong!');
+		}
+
+		const respData = await response.json();
+
+		dispatch({
+			type: ADD_ORDER,
+			orderData: {
+				id: respData.name,
+				items: cartItems,
+				amount: totalAmount,
+				date
+			}
+		});
+	};
+};
