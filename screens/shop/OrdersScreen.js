523d05b135f8a148bc82a3cc3603073d0427eb24
bud linville <budlinville@gmail.com>
2021-03-10 19:45:44 -0600

    Added fetching for stored orders

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -1,13 +1,34 @@
-import React from 'react';
-import { Text, FlatList, Platform } from 'react-native';
-import { useSelector }	from 'react-redux';
+import React, { useState, useEffect } from 'react';
+import { View, FlatList, Platform, ActivityIndicator, StyleSheet } from 'react-native';
+import { useSelector, useDispatch }	from 'react-redux';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import HeaderButton from '../../components/UI/HeaderButton';
 import OrderItem from '../../components/shop/OrderItem';
+import * as ordersActions from '../../store/actions/orders';
+import Colors from '../../constants/Colors';
 
 const OrdersScreen = props => {
+	const [isLoading, setIsLoading] = useState(false);
 	const orders = useSelector(state => state.orders.orders);
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		const fetchOrders = async () => {
+			setIsLoading(true);
+			await dispatch(ordersActions.fetchOrders());
+			setIsLoading(false);
+		};
+		fetchOrders();
+	}, [dispatch, setIsLoading, ordersActions]);
+
+	if (isLoading) {
+		return (
+			<View style={styles.centered}>
+				<ActivityIndicator size='large' color={Colors.primary} />
+			</View>
+		);
+	}
 
 	return <FlatList
 		data={orders}
@@ -34,6 +55,14 @@ OrdersScreen.navigationOptions = navData => ({
 			/>
 		</HeaderButtons>
 	),
+});
+
+const styles = StyleSheet.create({
+	centered: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center'
+	}
 });
 
 export default OrdersScreen;
--- store/actions/orders.js
+++ store/actions/orders.js
@@ -1,4 +1,30 @@
+import Order from '../../models/order';
+
 export const ADD_ORDER = 'ADD_ORDER';
+export const SET_ORDERS = 'SET_ORDERS';
+
+export const fetchOrders = () => {
+	return async dispatch => {
+		try {
+			const response = await fetch('https://rn-complete-guide-785b1-default-rtdb.firebaseio.com/orders/u1.json');
+			if (!response.ok) {
+				throw new Error('Something went wrong');
+			}
+
+			const respData = await response.json();
+			// id, items, totalAmount, date
+			const loadedOrders = respData && Object.entries(respData).map((entry) => new Order(
+				entry[0],	// key
+				entry[1].cartItems,
+				entry[1].totalAmount,
+				new Date(entry[1].date)
+			)) || [];
+			dispatch({ type: SET_ORDERS, orders: loadedOrders });
+		} catch (err) {
+
+		}
+	};
+};
 
 export const addOrder = (cartItems, totalAmount) => {
 	return async dispatch => {
